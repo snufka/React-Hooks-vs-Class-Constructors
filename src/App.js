ba86f66e3dbf76d4f597371f2a7cf8c73d379b7a
snufka <52801250+snufka@users.noreply.github.com>
2020-05-23 22:12:46 +0200

    hooks counter vs class constructor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Counter from './Counter'
+import CounterHooks from './CounterHooks'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      Counter
+      <Counter initialCount={0} />
+      Counter Hooks
+      <CounterHooks initialCount={7} />
+    </>
+  )
+
 }
 
 export default App;
